44c817dcde212ba0688b1ac773a5bd1a6b39e43b
kemal <atdayewkemal@gmail.com>
2023-11-17 13:57:32 +0500

    configuration structures has been made

---
+++ internal/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import "time"
+
+type Config struct {
+	Env string `yaml:"env"`
+}
+
+type Database struct {
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Sslmode  string `yaml:"sslmode"`
+}
+
+type HTTPServer struct {
+	Address string `yaml:"address"`
+	Timeout time.Duration `yaml:"timeout"`
+	Idle_Timeout time.Duration `yaml:"idle_timeout"`
+}
