21e432d9ac0daaa3b9a2d731c8424c53bbc5f4e1
kemal <atdayewkemal@gmail.com>
2023-11-18 20:03:36 +0500

    configs are loaded and loggers are enabled

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"user-admin/internal/config"
+	"user-admin/internal/logger"
+)
 
+func main() {
+	cfg := config.LoadConfig()
+
+	log := logger.SetupLogger(cfg.Env)
+
+	log.Info("Starting the server...", slog.String("env", cfg.Env))
+	log.Debug("Debug messages are enabled") // if env is set to prod, debug messages are going to be disabled
 }
