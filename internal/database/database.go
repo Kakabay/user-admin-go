900415145ccc429bb28989d5fe41f892645bfa8a
kemal <atdayewkemal@gmail.com>
2023-11-20 14:51:43 +0500

    database connection establishment function is added

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,7 +1,31 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"user-admin/internal/config"
 
-type Database struct {
-	db *sql.DB
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func InitDB(cfg *config.Config) (*sql.DB, error) {
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBname, cfg.Sslmode)
+
+	var err error 
+	db, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to initialize database: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("Failed to ping database: %v", err)
+	}
+
+	return db, nil
+}
+
+func GetDB() *sql.DB {
+	return db
 }
