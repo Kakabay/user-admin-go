dfee0654c22d53f27ff0527d01d8ca8d170bae77
kemal <atdayewkemal@gmail.com>
2023-11-21 16:08:38 +0500

    UserRepostory interface is added

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,13 @@
 package repository
+
+import "user-admin/internal/domain"
+
+type UserRepository interface {
+	GetAllUsers() (*domain.UsersList, error)
+	GetUserByID(id int32) (*domain.GetUserResponse, error)
+	CreateUser(request *domain.CreateUserRequest) (*domain.CreateUserResponse, error)
+	UpdateUser(request *domain.UpdateUserRequest) (*domain.UpdateUserResponse, error)
+	DeleteUser(id int32) error
+	BlockUser(id int32) error
+	UnblockUser(id int32) error
+}
