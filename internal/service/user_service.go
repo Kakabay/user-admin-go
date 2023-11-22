4ee91789f895e94b98c2a8073988aeefe107f01c
kemal <atdayewkemal@gmail.com>
2023-11-22 17:40:52 +0500

    passing parameters bugs are fixed

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -17,26 +17,26 @@ func (s *UserService) GetAllUsers() (*domain.UsersList, error) {
 	return s.UserRepository.GetAllUsers()
 }
 
-func (s *UserService) GetUserByID(id int32) (*domain.CommonUserResponse, error) {
-	return s.UserRepository.GetUserByID()
+func (s *UserService) GetUserByID(id int32) (*domain.GetUserResponse, error) {
+	return s.UserRepository.GetUserByID(id)
 }
 
-func (s *UserService) CreateUser(request *domain.CreateUserRequest) (*domain.CommonUserResponse, error) {
-	return s.UserRepository.CreateUser()
+func (s *UserService) CreateUser(request *domain.CreateUserRequest) (*domain.CreateUserResponse, error) {
+	return s.UserRepository.CreateUser(request)
 }
 
-func (s *UserService) UpdateUser(request *domain.UpdateUserRequest) (*domain.CommonUserResponse, error) {
-	return s.UserRepository.UpdateUser()
+func (s *UserService) UpdateUser(request *domain.UpdateUserRequest) (*domain.UpdateUserResponse, error) {
+	return s.UserRepository.UpdateUser(request)
 }
 
 func (s *UserService) DeleteUser(id int32) error {
-	return s.UserRepository.DeleteUser()
+	return s.UserRepository.DeleteUser(id)
 }
 
 func (s *UserService) BlockUser(id int32) error {
-	return s.UserRepository.BlockUser()
+	return s.UserRepository.BlockUser(id)
 }
 
 func (s *UserService) UnblockUser(id int32) error {
-	return s.UserRepository.UnblockUser()
+	return s.UserRepository.UnblockUser(id)
 }
