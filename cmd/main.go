2e156c0e80430cc0f739d227932d8a30f5888fbe
kemal <atdayewkemal@gmail.com>
2023-11-22 13:20:12 +0500

    Graceful shutdown handling is added

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"user-admin/internal/config"
 	"user-admin/pkg/database"
 	log_utils "user-admin/pkg/lib/logger_utils"
@@ -19,21 +21,36 @@ func main() {
 	log := logger.SetupLogger(cfg.Env)
 
 	log.Info("Starting the server...", slog.String("env", cfg.Env))
-	log.Debug("Debug messages are enabled") // if env is set to prod, debug messages are going to be disabled
+	log.Debug("Debug messages are enabled") // If env is set to prod, debug messages are going to be disabled
 
 	db, err := database.InitDB(cfg)
 	if err != nil {
 		log.Error("Failed to init database:", log_utils.Err(err))
 		os.Exit(1)
-	} 
+	}
 	defer db.Close()
 
 	router := chi.NewRouter()
 	router.Use(middleware.RequestID)
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
-	
+
 	log.Info("Starting the server...")
+
+	// Handle graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-stop
+		log.Info("Shutting down the server gracefully...")
+
+		if err := db.Close(); err != nil {
+			log.Error("Error closing database:", log_utils.Err(err))
+		}
+		os.Exit(0)
+	}()
+
 	err = http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Error("Server failed to start:", err)
