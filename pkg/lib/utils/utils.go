a00e9a29c9cd3beccb3e14d57bd17645fd766fd1
kemal <atdayewkemal@gmail.com>
2023-11-26 17:11:09 +0500

    NullIfEmptyStr and NullIfEmptyDate are added

--- pkg/lib/utils/utils.go
+++ pkg/lib/utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"database/sql"
 	"log/slog"
+	"user-admin/internal/domain"
 )
 
 // Use slog to handle errors
@@ -18,4 +19,18 @@ func HandleNullString(valid sql.NullString) string {
 		return valid.String
 	}
 	return ""
+}
+
+func NullIfEmptyStr(s string) sql.NullString {
+	if s == "" {
+		return sql.NullString{}
+	}
+	return sql.NullString{String: s, Valid: true}
+}
+
+func NullIfEmptyDate(d domain.Date) sql.NullTime {
+	if d == (domain.Date{}) {
+		return sql.NullTime{}
+	}
+	return sql.NullTime{Valid: true}
 }
