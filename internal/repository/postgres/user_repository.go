c61fa0912d10f7c7d0f21a4fab586355678bea47
kemal <atdayewkemal@gmail.com>
2023-11-27 15:15:55 +0500

    Checking user existance logic is added into DeleteUser, BlockUser and UnblockUser

--- internal/repository/postgres/user_repository.go
+++ internal/repository/postgres/user_repository.go
@@ -255,6 +255,17 @@ func (r PostgresUserRepository) DeleteUser(id int32) error {
 }
 
 func (r *PostgresUserRepository) BlockUser(id int32) error {
+	var exists bool
+	err := r.DB.QueryRow(`SELECT EXISTS(SELECT 1 FROM users WHERE id = $1)`, id).Scan(&exists)
+	if err != nil {
+		slog.Error("error checking user existence: %v", utils.Err(err))
+		return err
+	}
+
+	if !exists {
+		return fmt.Errorf("user with ID %d not found", id)
+	}
+
     stmt, err := r.DB.Prepare("UPDATE users SET blocked = true WHERE id = $1")
     if err != nil {
         slog.Error("error preparing query: %v", utils.Err(err))
