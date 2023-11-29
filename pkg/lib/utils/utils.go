23fd6afab38bac170a77620a528347d14f4b1288
kemal <atdayewkemal@gmail.com>
2023-11-29 23:31:51 +0500

    RespondWithError and RespondWithJSON functions are added

--- pkg/lib/utils/utils.go
+++ pkg/lib/utils/utils.go
@@ -2,17 +2,19 @@ package utils
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log/slog"
+	"net/http"
 	"strings"
 	"user-admin/internal/domain"
 )
 
 // Use slog to handle errors
 func Err(err error) slog.Attr {
-	return slog.Attr{
-		Key:   "error",
-		Value: slog.StringValue(err.Error()),
-	}
+    return slog.Attr{
+        Key:   "error",
+        Value: slog.StringValue(err.Error()),
+    }
 }
 
 func HandleNullString(valid sql.NullString) string {
@@ -40,4 +42,18 @@ func IsValidPhoneNumber(phoneNumber string) bool {
 	// Check if the phone number consists of 12 digits and starts with "+993"
 	const validPrefix = "+993"
 	return len(phoneNumber) == 12 && strings.HasPrefix(phoneNumber, validPrefix)
+}
+
+func RespondWithError(w http.ResponseWriter, code int, message string) {
+    w.WriteHeader(code)
+    w.Write([]byte(message))
+}
+
+func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    if err := json.NewEncoder(w).Encode(payload); err != nil {
+        slog.Error("Error encoding JSON: ", Err(err))
+        RespondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+    }
 }
