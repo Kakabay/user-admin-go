17a699e567e9951884ac72123e0ab610969b9252
kemal <atdayewkemal@gmail.com>
2024-01-03 15:17:07 +0500

    new functions are added

--- internal/repository/auth_repository.go
+++ internal/repository/auth_repository.go
@@ -1,10 +1,14 @@
 package repository
 
-import "user-admin/internal/domain"
+import (
+	"user-admin/internal/domain"
+)
 
 type AdminAuthRepository interface {
-	// Methods for authentication
 	GetAdminByUsername(username string) (*domain.Admin, error)
 	GenerateAccessToken(admin *domain.Admin) (string, error)
 	GenerateRefreshToken(admin *domain.Admin) (string, error)
+    ValidateRefreshToken(refreshToken string) (map[string]interface{}, error)
+    GetAdminByID(adminID int) (*domain.Admin, error)
+    DeleteRefreshToken(refreshToken string) error
 }
