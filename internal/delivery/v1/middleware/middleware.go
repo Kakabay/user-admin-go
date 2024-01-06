d0d7b72bb0527594c7b2f2de10072a90d0852da5
kemvl <atdayewkemal@gmail.com>
2024-01-06 17:56:59 +0500

    extra comments are removed

--- internal/delivery/v1/middleware/middleware.go
+++ internal/delivery/v1/middleware/middleware.go
@@ -21,57 +21,53 @@ const (
 	tokenKey contextKey = "token"
 )
 
-// AuthorizationMiddleware is a middleware function that performs authorization checks based on JWT tokens.
 func AuthorizationMiddleware(cfg *config.Config, requiredRoles []string) func(http.Handler) http.Handler {
-    return func(next http.Handler) http.Handler {
-        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            tokenString := extractTokenFromHeader(r)
-            if tokenString == "" {
-                utils.RespondWithError(w, http.StatusUnauthorized, "Authorization token not provided")
-                return
-            }
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			tokenString := extractTokenFromHeader(r)
+			if tokenString == "" {
+				utils.RespondWithError(w, http.StatusUnauthorized, "Authorization token not provided")
+				return
+			}
 
-            claims, err := validateToken(tokenString, cfg)
-            if err != nil {
-                utils.RespondWithError(w, http.StatusUnauthorized, fmt.Sprintf("Invalid authorization token: %v", err))
-                return
-            }
+			claims, err := validateToken(tokenString, cfg)
+			if err != nil {
+				utils.RespondWithError(w, http.StatusUnauthorized, fmt.Sprintf("Invalid authorization token: %v", err))
+				return
+			}
 
 			adminRole, ok := claims["role"].(string)
 			if !ok {
 				utils.RespondWithError(w, http.StatusUnauthorized, "Role not found in token claims")
 				return
 			}
-			
+
 			if !hasRequiredRole(adminRole, requiredRoles) {
 				utils.RespondWithError(w, http.StatusForbidden, "Insufficient permissions")
 				return
 			}
 
-            // Pass the claims to the next handler
-            ctx := r.Context()
-            ctx = context.WithValue(ctx, tokenKey, claims)
-            next.ServeHTTP(w, r.WithContext(ctx))
-        })
-    }
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, tokenKey, claims)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
 }
 
 func extractTokenFromHeader(r *http.Request) string {
-    bearerToken := r.Header.Get("Authorization")
-    if bearerToken == "" {
-        slog.Error("Authorization header not found")
-        return ""
-    }
+	bearerToken := r.Header.Get("Authorization")
+	if bearerToken == "" {
+		slog.Error("Authorization header not found")
+		return ""
+	}
 
-    // Check if the Authorization header has the expected "Bearer " prefix
-    if !strings.HasPrefix(bearerToken, "Bearer ") {
-        slog.Error("Invalid Authorization header format")
-        return ""
-    }
+	if !strings.HasPrefix(bearerToken, "Bearer ") {
+		slog.Error("Invalid Authorization header format")
+		return ""
+	}
 
-    return strings.TrimPrefix(bearerToken, "Bearer ")
+	return strings.TrimPrefix(bearerToken, "Bearer ")
 }
-
 
 func hasRequiredRole(adminRole string, requiredRoles []string) bool {
 	for _, requiredRole := range requiredRoles {
@@ -82,26 +78,24 @@ func hasRequiredRole(adminRole string, requiredRoles []string) bool {
 	return false
 }
 
-
-// validateToken validates and parses the JWT token.
 func validateToken(tokenString string, cfg *config.Config) (jwt.MapClaims, error) {
-    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-            return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-        }
-        return []byte(cfg.JWT.AccessSecretKey), nil
-    })
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(cfg.JWT.AccessSecretKey), nil
+	})
 
-    if err != nil || !token.Valid {
-        slog.Error("Token validation error: %v", err)
-        return nil, fmt.Errorf("token validation error: %v", err)
-    }
+	if err != nil || !token.Valid {
+		slog.Error("Token validation error: %v", err)
+		return nil, fmt.Errorf("token validation error: %v", err)
+	}
 
-    claims, ok := token.Claims.(jwt.MapClaims)
-    if !ok || claims == nil {
-        slog.Error("Invalid token claims")
-        return nil, fmt.Errorf("invalid token claims")
-    }
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || claims == nil {
+		slog.Error("Invalid token claims")
+		return nil, fmt.Errorf("invalid token claims")
+	}
 
-    return claims, nil
+	return claims, nil
 }
