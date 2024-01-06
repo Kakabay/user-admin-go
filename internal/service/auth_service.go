f7ee41171308bc477cde6bcd6fe353dad8f21cf2
kemvl <atdayewkemal@gmail.com>
2024-01-06 17:55:46 +0500

    refresh token logic has been added

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -10,68 +10,68 @@ import (
 )
 
 type AdminAuthService struct {
-    AdminAuthRepository repository.AdminAuthRepository
+	AdminAuthRepository repository.AdminAuthRepository
 }
 
 func NewAdminAuthService(adminAuthRepository repository.AdminAuthRepository) *AdminAuthService {
-    return &AdminAuthService{AdminAuthRepository: adminAuthRepository}
+	return &AdminAuthService{AdminAuthRepository: adminAuthRepository}
 }
 
 func (s *AdminAuthService) LoginAdmin(username, password string) (string, string, error) {
-    admin, err := s.AdminAuthRepository.GetAdminByUsername(username)
-    if err != nil {
-        slog.Error("Error getting admin by username:", utils.Err(err))
-        return "", "", err
-    }
+	admin, err := s.AdminAuthRepository.GetAdminByUsername(username)
+	if err != nil {
+		slog.Error("Error getting admin by username:", utils.Err(err))
+		return "", "", err
+	}
 
-    err = bcrypt.CompareHashAndPassword([]byte(admin.Password), []byte(password))
-    if err != nil {
-        slog.Error("Error comparing passwords:", utils.Err(err))
-        return "", "", domain.ErrInvalidCredentials
-    }
+	err = bcrypt.CompareHashAndPassword([]byte(admin.Password), []byte(password))
+	if err != nil {
+		slog.Error("Error comparing passwords:", utils.Err(err))
+		return "", "", domain.ErrInvalidCredentials
+	}
 
-    accessToken, err := s.AdminAuthRepository.GenerateAccessToken(admin)
+	accessToken, err := s.AdminAuthRepository.GenerateAccessToken(admin)
 	if err != nil {
 		slog.Error("Error generating access token:", utils.Err(err))
 		return "", "", err
 	}
 
-    refreshToken, err := s.AdminAuthRepository.GenerateRefreshToken(admin)
-    if err != nil {
-        slog.Error("Error generating refresh token:", utils.Err(err))
-        return "", "", err
-    }
+	refreshToken, err := s.AdminAuthRepository.GenerateRefreshToken(admin)
+	if err != nil {
+		slog.Error("Error generating refresh token:", utils.Err(err))
+		return "", "", err
+	}
 
-    return accessToken, refreshToken, nil
+	return accessToken, refreshToken, nil
 }
 
 func (s *AdminAuthService) RefreshTokens(refreshToken string) (string, error) {
-    claims, err := s.AdminAuthRepository.ValidateRefreshToken(refreshToken)
-    if err != nil {
-        slog.Error("Error validating refresh token:", utils.Err(err))
-        return "", err
-    }
+	claims, err := s.AdminAuthRepository.ValidateRefreshToken(refreshToken)
+	if err != nil {
+		slog.Error("Error validating refresh token:", utils.Err(err))
+		return "", err
+	}
 
-    adminIDFloat, ok := claims["adminID"].(float64)
-    if !ok {
-        slog.Error("AdminID not found or not a number in refresh token claims")
-        return "", domain.ErrInvalidRefreshToken
-    }
+	adminIDFloat, ok := claims["adminID"].(float64)
+	if !ok {
+		slog.Error("AdminID not found or not a number in refresh token claims")
+		return "", domain.ErrInvalidRefreshToken
+	}
 
-    // Convert adminID to int
-    adminID := int(adminIDFloat)
+	// Convert adminID to int
+	adminID := int(adminIDFloat)
 
-    admin, err := s.AdminAuthRepository.GetAdminByID(adminID)
-    if err != nil {
-        slog.Error("Error getting admin by ID:", utils.Err(err))
-        return "", err
-    }
+	admin, err := s.AdminAuthRepository.GetAdminByID(adminID)
+	if err != nil {
+		slog.Error("Error getting admin by ID:", utils.Err(err))
+		return "", err
+	}
 
-    newAccessToken, err := s.AdminAuthRepository.GenerateAccessToken(admin)
-    if err != nil {
-        slog.Error("Error generating access token:", utils.Err(err))
-        return "", err
-    }
+	newAccessToken, err := s.AdminAuthRepository.GenerateAccessToken(admin)
+	if err != nil {
+		slog.Error("Error generating access token:", utils.Err(err))
+		return "", err
+	}
 
-    return newAccessToken, nil
+	return newAccessToken, nil
 }
