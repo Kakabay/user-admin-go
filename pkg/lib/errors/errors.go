5c4e9493ded783e841369bf772279c454b09bd28
kemvl <atdayewkemal@gmail.com>
2024-01-07 12:30:10 +0500

    Comments and middleware related errors are added

--- pkg/lib/errors/errors.go
+++ pkg/lib/errors/errors.go
@@ -1,5 +1,6 @@
 package errors
 
+// auth
 const (
 	InvalidRequestFormat    = "Invalid request format"
 	AdminNotFound           = "Admin not found"
@@ -8,8 +9,16 @@ const (
 	InvalidRefreshToken     = "Invalid refresh token"
 )
 
+// user
 const (
 	InternalServerError = "Internal server error"
 	InvalidID           = "Invalid ID"
 	InvalidRequestBody  = "Invalid request body"
+)
+
+// middleware
+const (
+	AuthorizationTokenNotProvided = "Authorization token not provided"
+	RoleNotFoundInTokenClaims     = "Role not found in token claims"
+	InsufficientPermission        = "Insufficient permissions"
 )
